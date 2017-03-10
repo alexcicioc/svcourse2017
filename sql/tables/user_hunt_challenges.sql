1d440ff04f01a513f0e55d34122529933e41200e
dariusungur <darius.ungur@softvision.ro>
2017-03-10 17:12:26 +0200

    update default value start_time with current time stamp

--- sql/tables/user_hunt_challenges.sql
+++ sql/tables/user_hunt_challenges.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user_hunt_challenges` (
  `user_id` int(11) NOT NULL,
  `hunt_challenge_id` int(11) NOT NULL,
  `state` tinyint(2) NOT NULL,
- `start_time` datetime NOT NULL,
+ `start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
  `end_time` datetime NOT NULL,
  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
